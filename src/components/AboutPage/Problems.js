bfc5db788a9ce62f9feefcd1e5a4d48fdeb1be0f
Mehmet <mehmetsonmez216@gmail.com>
2023-12-21 23:33:56 +0300

    aboutus compenent added

---
+++ src/components/AboutPage/Problems.js
@@ -0,0 +1,29 @@
+const Problems = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <p>Problems trying</p>
+            <p>
+              Met minim Mollie non desert Alamo est sit cliquey dolor do met
+              sent.
+            </p>
+          </div>
+          <div>
+            Problems trying to resolve the conflict between the two major realms
+            of Cassical physics: Newtonian mechanics
+          </div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Problems;
